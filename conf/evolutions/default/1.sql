08f71c6b703df3e07cc2380eeb9cf94570197067
Valentin Rublack <valentin.rublack@gmail.com>
2016-11-28 01:01:16 -0500

    Included IntelliJ-specific files

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,62 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table business (
+  bemail                        varchar(255) not null,
+  bname                         varchar(255),
+  bdescription                  varchar(255),
+  catid                         integer,
+  password                      varchar(255),
+  constraint pk_business primary key (bemail)
+);
+
+create table category (
+  catid                         integer not null,
+  cname                         varchar(255),
+  cdescription                  varchar(255),
+  constraint pk_category primary key (catid)
+);
+create sequence category_seq;
+
+create table coupon (
+  id                            integer not null,
+  name                          varchar(255),
+  description                   varchar(255),
+  promocode                     varchar(255),
+  discount_rate                 float,
+  expiration_date               varchar(255),
+  bemail                        varchar(255),
+  constraint pk_coupon primary key (id)
+);
+create sequence coupon_seq;
+
+create table favorite (
+  key                           varchar(255) not null,
+  cookie                        varchar(255),
+  couponid                      integer,
+  constraint pk_favorite primary key (key)
+);
+
+create table user (
+  cookie                        varchar(255) not null,
+  location                      varchar(255),
+  constraint pk_user primary key (cookie)
+);
+
+
+# --- !Downs
+
+drop table if exists business;
+
+drop table if exists category;
+drop sequence if exists category_seq;
+
+drop table if exists coupon;
+drop sequence if exists coupon_seq;
+
+drop table if exists favorite;
+
+drop table if exists user;
+
